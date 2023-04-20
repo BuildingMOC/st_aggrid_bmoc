bc79deb93a1a7b939c4772a7792e76429d7f75ce
Kyle Wengryn <105943715+KyleWengrynBMOC@users.noreply.github.com>
2023-04-20 17:44:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ import setuptools
 
 setuptools.setup(
                  name = 'st_aggrid_bmoc'
-                 , version = '0.0.4'
+                 , version = '0.0.5'
                  , author = 'Raymond Carlson'
                  , author_email = 'raymondac@buildingmoc.com'
                  , description = 'Various Tools for use with BMOC Data'
